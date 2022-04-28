24cf9938a5a9fd516f26a20a9116b6dc5cfae989
claudione996 <s295829@studenti.polito.it>
2022-04-28 12:57:43 +0200

    update sidebar

--- biglab1/src/SidebarComponents.js
+++ biglab1/src/SidebarComponents.js
@@ -1,15 +1,16 @@
-function Sidebar(props){
-    return(
-        <aside>
-            <div class="list-group list-group-flush">
-                <a href="#" class="list-group-item list-group-item-action active" onClick={()=>{props.filter('All')}}>All</a>
-                <a href="#" class="list-group-item list-group-item-action" onClick={()=>{props.filter('Favorites')}}>Favorites</a>
-                <a href="#" class="list-group-item list-group-item-action" onClick={()=>{props.filter('Best Rated')}}>Best Rated</a>
-                <a href="#" class="list-group-item list-group-item-action" onClick={()=>{props.filter('Seen Last Month')}}>Seen Last Month</a>
-                <a href="#" class="list-group-item list-group-item-action" onClick={()=>{props.filter('Unseen')}}>Unseen</a>  
-            </div>
-      </aside>
+import Nav from 'react-bootstrap/Nav'
+function Sidebar(props) {
+    return (
+        
+    <Nav defaultActiveKey="1" className="flex-column">
+        <Nav.Link className="list-group-item list-group-item-action" eventKey="1" onClick={() => { props.filter('All') }}>All</Nav.Link>
+        <Nav.Link className="list-group-item list-group-item-action" eventKey="2" onClick={() => { props.filter('Favorites') }}>Favorites</Nav.Link>
+        <Nav.Link className="list-group-item list-group-item-action" eventKey="3" onClick={() => { props.filter('Best Rated') }}>Best Rated</Nav.Link>
+        <Nav.Link className="list-group-item list-group-item-action" eventKey="4" onClick={() => { props.filter('Seen Last Month') }}>Seen Last Month</Nav.Link>
+        <Nav.Link className="list-group-item list-group-item-action" eventKey="5" onClick={() => { props.filter('Unseen') }}>Unseen</Nav.Link>
+    </Nav>
+    
     );
-} 
+}
 
-export {Sidebar};
+export { Sidebar };
