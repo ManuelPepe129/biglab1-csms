290f91a8e8687fbcf1d0fb5e096d1b50e1a949d4
claudione996 <s295829@studenti.polito.it>
2022-04-28 17:00:37 +0200

    update stars rating

--- biglab1/src/FilmsComponents.js
+++ biglab1/src/FilmsComponents.js
@@ -1,95 +1,109 @@
 import { useState } from 'react';
-import { Row, Col, Button} from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import React from 'react';
 import { times } from 'underscore'
 import _default from 'react-bootstrap/esm/Accordion';
 import { Sidebar } from './SidebarComponents';
 //import './FilmsComponents.css';
-import {Star,StarFill} from 'react-bootstrap-icons';
+//import { Star, StarFill } from 'react-bootstrap-icons';
 import dayjs from 'dayjs';
 
-function MainComponent(props){
-    const [filter, setFilter] = useState("All");
+import ReactStars from 'react-stars'
 
-    function changeFilter(fil){
-      setFilter(fil);
-    }
-    return(
-      <Row>
-        <Col xs={3}>      
-          <Sidebar filter={changeFilter}/>
-        </Col>
-        <Col xs={9}>
-          <Films films = {props.films} filter={filter} />
-        </Col> 
+function MainComponent(props) {
+  const [filter, setFilter] = useState("All");
 
-      </Row>
-    );
+  function changeFilter(fil) {
+    setFilter(fil);
+  }
+  return (
+    <Row>
+      <Col xs={3}>
+        <Sidebar filter={changeFilter} />
+      </Col>
+      <Col xs={9}>
+        <Films films={props.films} filter={filter} />
+      </Col>
+
+    </Row>
+  );
 
 }
 
 
-function Films(props){
-    const [films, setFilms] = useState(props.films);
-    return(
-        <>
-        <h1 class="mb-2" id="filter-title">{props.filter}</h1>
-        <ul class="list-group list-group-flush" id="list-films">
+function Films(props) {
+  const [films, setFilms] = useState(props.films);
+  return (
+    <>
+      <h1 class="mb-2" id="filter-title">{props.filter}</h1>
+      <ul class="list-group list-group-flush" id="list-films">
         {
-          films.filter(f=>{switch(props.filter)
-                        {
-                            case 'Favorites':
-                             return f.isFavourite;
-                              
-                            case 'Best Rated':
-                              return f.rating==5;
-                              
-                            case 'Seen Last Month':
-                              if(f.date!=undefined)
-                             return f.date.isAfter(dayjs().subtract(30,'day'));
-                              else return false;
-                            case 'Unseen':
-                             return f.date==undefined;
-                              
-                            default:
-                              return true;
-                        }}).map((f) => <FilmData film={f} key={f.id} />)
+          films.filter(f => {
+            switch (props.filter) {
+              case 'Favorites':
+                return f.isFavourite;
+
+              case 'Best Rated':
+                return f.rating == 5;
+
+              case 'Seen Last Month':
+                if (f.date != undefined)
+                  return f.date.isAfter(dayjs().subtract(30, 'day'));
+                else return false;
+              case 'Unseen':
+                return f.date == undefined;
+
+              default:
+                return true;
+            }
+          }).map((f) => <FilmData film={f} key={f.id} />)
         }
-        </ul>
-        
-        <Button className="btn btn-lg btn-primary fixed-right-bottom">&#43;</Button>
-       
-        </>
-    );
+      </ul>
+
+      <Button className="btn btn-lg btn-primary fixed-right-bottom">&#43;</Button>
+
+    </>
+  );
 }
 
-function FilmData(props){
-    return(
-        <>
-        <li class="list-group-item">
-            <div class="d-flex w-100 justify-content-between">
-              <p class="favorite text-start col-5">{props.film.title}</p>
-              <span class="custom-control custom-checkbox col-3">
-                { (props.film.isFavourite) ?
-                    <input type="checkbox" class="custom-control-input" id="check" checked/>
-                    : <input type="checkbox" class="custom-control-input" id="check"/>
-                }
-                <label class="custom-control-label" for="check"> Favorite</label>
-              </span>
-              <p class="col-2">{(props.film.date != undefined) ? props.film.date.format('YYYY-MM-DD') : ""}</p>
-              <div class = "col-3">
-              { times(5, (i) => (
+function FilmData(props) {
+
+  const ratingChanged = (newRating) => {
+    props.film.rating=newRating;
+  }
+  return (
+    <>
+      <li class="list-group-item">
+        <div class="d-flex w-100 justify-content-between">
+          <p class="favorite text-start col-5">{props.film.title}</p>
+          <span class="custom-control custom-checkbox col-3">
+            {(props.film.isFavourite) ?
+              <input type="checkbox" class="custom-control-input" id="check" checked />
+              : <input type="checkbox" class="custom-control-input" id="check" />
+            }
+            <label class="custom-control-label" for="check"> Favorite</label>
+          </span>
+          <p class="col-2">{(props.film.date != undefined) ? props.film.date.format('YYYY-MM-DD') : ""}</p>
+          <div class="col-3">
+            {/* times(5, (i) => (
                 (props.film.rating != undefined) ?
                     (props.film.rating > i) ? <StarFill/> : <Star/>
                     : ""
-              ))} 
-              
-              </div>              
-            </div>
-          </li>
+              ))*/}
+            <ReactStars
+              value={props.film.rating}
+              count={5}
+              edit={true}
+              half={false}
+              onChange={ratingChanged}
+              size={24}
+              color2={'#ffd700'} />
+          </div>
+        </div>
+      </li>
 
-        </>
-    );
+    </>
+  );
 }
 
 
