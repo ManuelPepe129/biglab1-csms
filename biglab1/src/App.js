0af263180d1c63b9e0f33e6c5564b67f9db53f2a
claudione996 <s295829@studenti.polito.it>
2022-04-28 12:38:13 +0200

    visualizzazione con filtri

--- biglab1/src/App.js
+++ biglab1/src/App.js
@@ -8,7 +8,7 @@ import { MainComponent } from './FilmsComponents';
 
 const filmList = [
   {id: 1, title: "Pulp Fiction", isFavourite: true, date: dayjs('2022-03-10'), rating: 5},
-  {id: 2, title: "21 Grams", isFavourite: true, date: dayjs('2022-01-17'), rating: 5},
+  {id: 2, title: "21 Grams", isFavourite: true, date: dayjs('2022-04-17'), rating: 5},
   {id: 3, title: "Star Wars", isFavourite: false, date: undefined, rating: undefined},
   {id: 4, title: "Matrix", isFavourite: false, date: undefined, rating: undefined},
   {id: 5, title: "Shrek", isFavourite: false, date: dayjs('2021-11-21'), rating: 3}
--- biglab1/src/SidebarComponents.js
+++ biglab1/src/SidebarComponents.js
@@ -1,12 +1,12 @@
-function Sidebar(){
+function Sidebar(props){
     return(
         <aside>
             <div class="list-group list-group-flush">
-                <a href="#" class="list-group-item list-group-item-action active">All</a>
-                <a href="#" class="list-group-item list-group-item-action">Favorites</a>
-                <a href="#" class="list-group-item list-group-item-action">Best Rated</a>
-                <a href="#" class="list-group-item list-group-item-action">Seen Last Month</a>
-                <a href="#" class="list-group-item list-group-item-action">Unseen</a>    
+                <a href="#" class="list-group-item list-group-item-action active" onClick={()=>{props.filter('All')}}>All</a>
+                <a href="#" class="list-group-item list-group-item-action" onClick={()=>{props.filter('Favorites')}}>Favorites</a>
+                <a href="#" class="list-group-item list-group-item-action" onClick={()=>{props.filter('Best Rated')}}>Best Rated</a>
+                <a href="#" class="list-group-item list-group-item-action" onClick={()=>{props.filter('Seen Last Month')}}>Seen Last Month</a>
+                <a href="#" class="list-group-item list-group-item-action" onClick={()=>{props.filter('Unseen')}}>Unseen</a>  
             </div>
       </aside>
     );
