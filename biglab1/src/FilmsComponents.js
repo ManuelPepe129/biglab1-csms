814349c0b93c8442fae6d5f86fb247ee19a24362
Sofia Belloni <sofia.belloni@studenti.polito.it>
2022-04-29 10:41:46 +0200

    Fix Favorite Logica

--- biglab1/src/FilmsComponents.js
+++ biglab1/src/FilmsComponents.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Form} from 'react-bootstrap';
+import FormCheck  from 'react-bootstrap/FormCheck'
 import React from 'react';
-import _default from 'react-bootstrap/esm/Accordion';
 import { Sidebar } from './SidebarComponents';
 import './FilmsComponents.css';
 
@@ -43,14 +43,14 @@ function Films(props) {
                 return f.isFavourite;
 
               case 'Best Rated':
-                return f.rating == 5;
+                return f.rating === 5;
 
               case 'Seen Last Month':
-                if (f.date != undefined)
+                if (f.date !== undefined)
                   return f.date.isAfter(dayjs().subtract(30, 'day'));
                 else return false;
               case 'Unseen':
-                return f.date == undefined;
+                return f.date === undefined;
 
               default:
                 return true;
@@ -70,25 +70,22 @@ function FilmData(props) {
   const ratingChanged = (newRating) => {
     props.film.rating=newRating;
   }
+
+  const favoriteChanged = (oldValue) => {
+    props.film.isFavourite = !oldValue;
+  }
+
   return (
     <>
       <li class="list-group-item">
         <div class="d-flex w-100 justify-content-between">
           <p class="favorite text-start col-5">{props.film.title}</p>
           <span class="custom-control custom-checkbox col-3">
-            {(props.film.isFavourite) ?
-              <input type="checkbox" class="custom-control-input" id="check" checked />
-              : <input type="checkbox" class="custom-control-input" id="check" />
-            }
+            <input type="checkbox" id="check" class="custom-control-input" onChange={()=>favoriteChanged(props.film.isFavourite)} checked={props.film.isFavourite}/>
             <label class="custom-control-label" for="check"> Favorite</label>
           </span>
-          <p class="col-2">{(props.film.date != undefined) ? props.film.date.format('YYYY-MM-DD') : ""}</p>
+          <p class="col-2">{(props.film.date !== undefined) ? props.film.date.format('YYYY-MM-DD') : ""}</p>
           <div class="col-3">
-            {/* times(5, (i) => (
-                (props.film.rating != undefined) ?
-                    (props.film.rating > i) ? <StarFill/> : <Star/>
-                    : ""
-              ))*/}
             <ReactStars
               value={props.film.rating}
               count={5}
